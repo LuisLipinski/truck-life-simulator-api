9a01fd0b02ebbe41a8195a176930cea10127b4dd
LuisLipinski <98597288+LuisLipinski@users.noreply.github.com>
2026-08-24 12:53:30 -0300

    fix: ajustar PostgreSQLContainer não genérico

--- src/test/java/com/luislipinski/trucklife/identity/api/SessionLifecycleIntegrationTest.java
+++ src/test/java/com/luislipinski/trucklife/identity/api/SessionLifecycleIntegrationTest.java
@@ -63,7 +63,7 @@ class SessionLifecycleIntegrationTest {
 
     @Container
     @ServiceConnection
-    static final PostgreSQLContainer<?> POSTGRES = new PostgreSQLContainer<>(
+    static final PostgreSQLContainer POSTGRES = new PostgreSQLContainer(
             DockerImageName.parse("postgres:18-alpine")
     );
 
